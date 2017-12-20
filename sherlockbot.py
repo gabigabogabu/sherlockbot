4e2320c62bee2b8e5472e3c28e1a7351a3139e0f
Gabriel Schulze <gaschu95@gmail.com>
2017-12-20 03:33:12 +0100

    v0.0.0 reacts on mention. Gets karma by comment/submission and subreddit. Gets account age. Gets top submissions and comments.

---
+++ sherlockbot.py
@@ -0,0 +1,206 @@
+import praw     # python reddit api wrapper
+import datetime # account age etc
+import logging  # logging
+import re       # regex to extrect usernames
+import prawcore # for exception handling
+import requests # for exception handling
+import urllib3  # for exception handling
+from time import sleep  # retry throtteling
+from threading import Thread    # multithreading
+from praw.models import Comment # classify items as comment
+from praw.models import Submission  # classify items as submission
+
+__author__ = 'github.com/gaschu95'
+__version__ = '0.0.1'
+
+def get_logger():
+    'returns a logger that writes to console and to file'
+    log = logging.getLogger('_name_')
+    logFormat = '[%(asctime)s] [%(threadName)s] [%(levelname)s] - %(message)s'
+
+    # write to console
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter(logFormat))
+    log.addHandler(handler)
+
+    # write to file
+    handler = logging.FileHandler('sherlockbot.log')
+    handler.setFormatter(logging.Formatter(logFormat))
+    log.addHandler(handler)
+
+    log.setLevel(level=logging.DEBUG)
+    return log
+
+def check_on_mention(reddit, log):
+    'checks for new mentions and launches background checks'
+    log.info('checking inbox')
+
+    threads = []
+    while True:
+        try:
+            # get mentions and iterate over them
+            for item in reddit.inbox.stream():
+                log.debug(str(item) + ' found')
+
+                # get text of submission/comment
+                text = ''
+                if isinstance(item, Comment):
+                    text = item.body
+                if isinstance(item, Submission):
+                    text = item.selftext
+
+                # extract users to launch checks on
+                users = []
+                # find mentions in text
+                e = re.compile("u/[-_0-9a-zA-Z]*")
+                for m in e.finditer(text):
+                    users.append(reddit.redditor(m.group()[2:]))
+
+                # if noone mentioned check author and parent's author
+                if not users:
+                    if isinstance(item, Comment): users.append(item.parent().author)
+                    users.append(item.author)
+
+                # don't do checks on itself
+                users.remove(reddit.user.me())
+                # launch check for each mention
+                for u in users:
+                    t = Thread(target=background_check, args=(reddit, log, u, item), name=str(item)+'-Thread')
+                    threads.append(t)
+                    t.start()
+
+                # mark as read, so it does not reappear after relaunches
+                item.mark_read()
+
+                # clean up
+                threads = [t for t in threads if t.is_alive()]
+        except (prawcore.exceptions.RequestException,
+                requests.exceptions.ReadTimeout,
+                urllib3.exceptions.ReadTimeoutError) as e:
+                # Handle timeouts etc
+            # log error
+            logger.error(e)
+            # sleep
+            sleep(1)
+            # repeat
+            continue
+
+def background_check(reddit, log, user, comment):
+    'handle background check and posting'
+    msg = do_background_check(reddit, log, user)
+    post_results(reddit, log, comment, msg)
+
+def do_background_check(reddit, log, user):
+    'does actual background check'
+    log.info('doing background check for u/{}'.format(user.name))
+
+    # config
+    topCommentsInSubount = 5
+    topCommentCount = 5
+
+    # background check
+
+    # basic info
+    msg = ''
+    msg += 'u/' + user.name + '    \n'
+    msg += 'link_karma:\t' + str(user.link_karma) + '    \n'
+    msg += 'comment_karma:\t' + str(user.comment_karma) + '    \n'
+
+    # account age
+    account_created = datetime.datetime.fromtimestamp(user.created)
+    account_age = datetime.datetime.now() - datetime.datetime.fromtimestamp(user.created)
+    msg += 'account opened:\t' + str(account_created) + '    \n'
+    msg += 'account age:\t' + str(account_age) + '    \n'
+
+    # users most frequent subreddits
+    commentCount = 0 # how many comments were made
+    CommentsInSub = {} # subreddits and how many comments were made
+    CommentKarmaInSub = {} # subreddit and how much karma was earned
+    for c in user.comments.top(limit=None):
+        commentCount += 1
+        if c.subreddit.display_name in CommentKarmaInSub.keys():
+            CommentKarmaInSub[c.subreddit.display_name] += 1
+        else:
+            CommentKarmaInSub[c.subreddit.display_name] = 1
+
+        if c.subreddit.display_name in CommentsInSub.keys():
+            CommentsInSub[c.subreddit.display_name] += c.score
+        else:
+            CommentsInSub[c.subreddit.display_name] = c.score
+
+    submissionCount = 0 # how many submissions were made
+    submissionsInSub = {} # subreddits and how many submissions were made
+    submissionKarmaInSub = {} # subreddit and how much karma was earned
+    for s in user.submissions.top(limit=None):
+        submissionCount += 1
+        if s.subreddit.display_name in submissionsInSub.keys():
+            submissionsInSub[s.subreddit.display_name] += 1
+        else:
+            submissionsInSub[s.subreddit.display_name] = 1
+
+        if s.subreddit.display_name in submissionKarmaInSub.keys():
+            submissionKarmaInSub[s.subreddit.display_name] += s.score
+        else:
+            submissionKarmaInSub[s.subreddit.display_name] = s.score
+
+    submissionKarmaInSub = dict([(s, submissionKarmaInSub[s]) for s in sorted(submissionKarmaInSub, key=submissionKarmaInSub.get, reverse=True)][:topCommentsInSubount])
+    submissionsInSub = dict([(s, submissionsInSub[s]) for s in sorted(submissionsInSub, key=submissionsInSub.get, reverse=True)][:topCommentsInSubount])
+    msg += 'most submission karma in:\t' + str(submissionKarmaInSub)  + '    \n'
+    msg += 'most submissions in:\t' + str(submissionsInSub)  + '    \n'
+
+    CommentKarmaInSub = dict([(s, CommentKarmaInSub[s]) for s in sorted(CommentKarmaInSub, key=CommentKarmaInSub.get, reverse=True)][:topCommentsInSubount])
+    CommentsInSub = dict([(s, CommentsInSub[s]) for s in sorted(CommentsInSub, key=CommentsInSub.get, reverse=True)][:topCommentsInSubount])
+    msg += 'most comment karma in:\t' + str(CommentKarmaInSub)  + '    \n'
+    msg += 'most comments in:\t' + str(CommentsInSub)  + '    \n'
+
+    # TODO: get subreddit moderated by user
+    # # moderator of
+    # modSubs = []
+    # for s in submissionsInSub:
+    #     if user in reddit.subreddit(s).moderator():
+    #         modSubs.append(reddit.subreddit(s))
+    #
+    # modSubs = modSubs
+    # msg += 'moderates:\t{}    \n'.format(str(modSubs))
+
+    # users top submissions
+    msg += '\ntop submissions:    \n\n'
+    msg += 'Sub|Points|text\n'
+    msg += '--|--|--\n'
+    for s in user.submissions.top(limit=topCommentCount):
+        if s.selftext == '' or s.selftext == None:
+            msg += s.subreddit.display_name + ' | ' + str(s.score) + ' | [' + s.title + '](' + s.url + ')\n'
+        else:
+            msg += s.subreddit.display_name + ' | ' + str(s.score) + ' | ' + s.selftext.replace('\n', ' ') + '\n'
+
+    # users top comments
+    msg += '\ntop comments:    \n\n'
+    msg += 'Sub|Points|text\n'
+    msg += '--|--|--\n'
+    for c in user.comments.top(limit=topCommentCount):
+        msg += c.subreddit.display_name + ' | ' + str(c.score) + ' | ' + c.body.replace('\n', ' ') + '\n'
+
+    msg += '---\n^(I am a bot v{}. This message was created at {}.)'.format(__version__, str(datetime.datetime.now()))
+
+
+    # ideas:
+    # scan users comments for personal info
+    # search for twitter user with same name
+    # remember how often user was checked and also post that
+    return msg
+
+def post_results(reddit, log, comment, msg):
+    'submit comment with background check results'
+    log.info('replying results to comment {}'.format(str(comment)))
+    comment.reply(msg)
+
+def main():
+    log = get_logger()
+    log.info('logging in...')
+    r = praw.Reddit('sherlockbot')
+    mentionThread = Thread(target=check_on_mention, args=(r,log), name='mentionThread')
+    mentionThread.start()
+    # TODO: detect when to do checks on its own without being called
+
+if __name__ == '__main__':
+    main()
